9a017d799ab093894d4b7864ed2b9524a2292899
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-10-20 13:00:32 +0300

    Fetching and Displaying Data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [results, setResults] = React.useState([])
+
+  React.useEffect(() => {
+    fetch("http://localhost:3001/categories")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setResults(data);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {results.map(d => (<div key={d.id}>{d.title}</div>))}
     </div>
   );
 }
