1f8f499217c6265c3b383251b8924f99a0930aba
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-10-22 18:27:18 +0300

    Product Details and Description

--- src/components/productDetail.js
+++ src/components/productDetail.js
@@ -3,19 +3,74 @@ import { useParams } from "react-router-dom";
 import { getProductById } from "../fetch";
 
 const ProductDetail = () => {
-    const [product, setProduct] = React.useState({ errorMessage: "", data: [] });
+    const [product, setProduct] = React.useState({ errorMessage: "", data: {} });
     const { productId } = useParams();
 
     React.useEffect(() => {
         const fetchData = async () => {
             const responseObject = await getProductById(productId);
             setProduct(responseObject);
+            console.log(product);
         }
         fetchData();
     }, [productId]);
 
     return (
-        <div>productDetail Title: {product.data.title}</div>
+        <article>
+
+            <div className="category-product-title">
+                {product.data.title}
+            </div>
+
+            <figure>
+                <div className="category-product-image-container">
+                    <img src={`/assets/${product.data.image}`} alt={product.data.title} />
+                </div>
+            </figure>
+
+            <aside>
+                <div className="category-product-info-dimensions">
+                    <h3>Dimensions</h3>
+                    <label>{product.data.specs?.dimensions}</label>
+                </div>
+
+                {product.data.specs?.capacity &&
+                    <div className="category-product-info-capacity">
+                        <h3>Capacity</h3>
+                        <label>{product.data.specs?.capacity}</label>
+                    </div>
+                }
+
+                <div className="category-product-info-features">
+                    <h3>Features</h3>
+                    <ul>
+                        {product.data.features?.map((f, i) => {
+                            return <li key={`feature${i}`}>{f}</li>
+                        })}
+                    </ul>
+                </div>
+            </aside>
+
+            <aside className="category-product-finance">
+                <div className="category-product-finance-price">
+                    &pound;{product.data.price}
+                </div>
+
+                <div className="category-product-info-stock">
+                    <label>Stock level: {product.data.stock}</label>
+                    <label>FREE Delivery</label>
+                </div>
+
+                <div className="category-product-action">
+                    <button>Add to Basket</button>
+                </div>
+            </aside>
+
+            <div>
+                {product.data?.description}
+            </div>
+
+        </article>
     )
 }
 
