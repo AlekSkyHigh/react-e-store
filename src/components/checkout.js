429f4aeb16aa75d0ec9b136a6fab1457c79b9a7b
Aleksandar Voynov <aleksandar.voynov.info@gmail.com>
2023-11-02 16:38:40 +0200

    Implementing the Checkout Page

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,8 +1,60 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Checkout = () => {
+
+  const navigate = useNavigate();
+
+  const confirmOrder = (ev) => {
+    navigate("/orderconfirmation")
+  }
+
+
   return (
-    <div>Checkout</div>
+    <div className='checkout-container'>
+
+      <h2 className='checkout-title'>Shopping Checkout</h2>
+
+      <div className='checkout-header'>
+        <h4>Your Details</h4>
+      </div>
+
+      <hr className='checkout-headerline'></hr>
+
+      <div className='checkout-table'>
+        <label className='checkout-form-label'>Name</label>
+        <input type='text' name='name'></input>
+        <label className='checkout-form-label'>Email</label>
+        <input type='text' name='email'></input>
+      </div>
+
+      <div className='checkout-header'>
+        <h4>Adress Details</h4>
+      </div>
+
+      <hr className='checkout-headerline'></hr>
+
+      <div className='checkout-table'>
+        <label className='checkout-form-label'>Copy to Shipping</label>
+        <input className='checkout-form-checkbox' type='checkbox'></input>
+        <label className='checkout-form-label'>Billing Adress</label>
+        <div className='checkout-address'>
+          <input type='text' name='billing-address1'></input>
+          <input type='text' name='billing-address2'></input>
+          <input type='text' name='billing-city'></input>
+        </div>
+        <label className='checkout-form-label'>Shipping Adress</label>
+        <div className='checkout-address'>
+          <input type='text' name='shipping-address1'></input>
+          <input type='text' name='shipping-address2'></input>
+          <input type='text' name='shipping-city'></input>
+        </div>
+      </div>
+
+      <button className='cancel-button' onClick={() => navigate("/basket")}>Cancel</button>
+      <button className='checkout-button' onClick={confirmOrder}>Confirm Order</button>
+
+    </div>
   )
 }
 
